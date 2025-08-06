7ce940d08b8999c96a15c38f2d026b5c162991b4
VyshnaviBhongiri <2420090026csit@gmail.com>
2025-08-06 11:14:53 +0530

    Add files via upload

---
+++ Project1/src/skill1/StudentDashboard.java
@@ -0,0 +1,19 @@
+package skill1;
+public class StudentDashboard {
+	    public static void main(String[] args) {
+	        long rollNumber = 2420090026L;
+	        String name = "Vaishnavi";
+	        float marks = 89.5f;
+	        char gender = 'F';
+	        boolean isPass = true;
+
+	        System.out.println("  ** Student Summary**  ");
+	        System.out.println("Roll Number: " + rollNumber);
+	        System.out.println("Name: " + name);
+	        System.out.println("Marks: " + marks);
+	        System.out.println("Gender: " + gender);
+	        System.out.println("Pass Status: " + isPass);
+	    }
+	}
+
+
